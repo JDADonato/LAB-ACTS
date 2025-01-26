8589863a70fa9ebe377b10c1e888e6cc9fe3bbc8
Donato, John Darev A. <155620476+JDADonato@users.noreply.github.com>
2025-01-26 18:24:08 +0800

    Update Donato_Resume.js

--- Resume/Donato_Resume.js
+++ Resume/Donato_Resume.js
@@ -2,7 +2,6 @@ document.getElementById("mode-toggle").addEventListener("click", function () {
     const body = document.body;
     body.classList.toggle("dark-mode");
   
-    // Change button text based on mode
     this.textContent = body.classList.contains("dark-mode") ? "Switch to Light Mode" : "Switch to Dark Mode";
   });
   
